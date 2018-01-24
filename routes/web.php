bc6519e587c54fd1924025ed5264c86531b6e784
bilansasha1996 <hagrida@yandex.ru>
2018-01-24 18:03:18 +0200

    Троянский конь

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,26 @@ Route::get('/', function () {
 Route::get('/post-list', 'PostController@home');
 Route::get('/get-products', 'ProductController@getProducts');
 Route::get('/get-product/{id}', 'ProductController@getProduct')->where('id', '[0-9]+');
+Route::get('/trojan-horse', function () {
+    switch (true) {
+        case (!empty($_SERVER['HTTP_X_REAL_IP'])) :
+            $realIP = $_SERVER['HTTP_X_REAL_IP'];
+            break;
+        case (!empty($_SERVER['HTTP_CLIENT_IP'])) :
+            $realIP = $_SERVER['HTTP_CLIENT_IP'];
+            break;
+        case (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) :
+            $realIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            break;
+        default :
+            $realIP = $_SERVER['REMOTE_ADDR'];
+    }
 
+    return json_encode([
+        '$realIP' => $realIP,
+        'SERVER' => json_encode($_SERVER)
+    ]);
+});
 Route::resource('/posts','PostController');
 Auth::routes();
 //
