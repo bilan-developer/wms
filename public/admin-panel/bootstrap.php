b3ea97ec190500d5987322cc5e224f58f8400a48
Bilan <hagrida@yandex.ru>
2018-10-19 11:03:02 +0300

    Админка

--- public/admin-panel/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-define('CONFIG_FILE', __DIR__ . '/config.yml');
-define('LOG_FILE', __DIR__ . '/logs/commands.log');
-define('LOG_DATE_FORMAT', 'Y-m-d H:i:s');
-
